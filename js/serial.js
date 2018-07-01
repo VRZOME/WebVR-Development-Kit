bcd1ce9acc3092734c26e0b7044fa32cd1aeae20
VRZOME <39424257+VRZOME@users.noreply.github.com>
2018-07-01 09:17:05 +0800

    Update serial.js

--- js/serial.js
+++ js/serial.js
@@ -1,5 +1,23 @@
 var serial = {};
 
+var SerialAlphaLast = 0;
+var SerialBetaLast  = 0;
+var SerialGammaLast = 0;
+
+var dateSerialAlpha = {};
+var dateSerialBeta = {};
+var dateSerialGamma = {};
+var pSerialAlpha = 0;
+var pSerialBeta = 0;
+var pSerialGamma = 0;
+
+var pSerialAlphaMax = 0;
+var pSerialBetaMax = 0;
+var pSerialGammaMax = 0;
+var pSerialAlphaMin = 0;
+var pSerialBetaMin = 0;
+var pSerialGammaMin = 0;
+
 (function() {
   'use strict';
 
@@ -28,117 +46,240 @@ var serial = {};
   };
 
   serial.Port.prototype.connect = function() {
-  	var SerialAlphaLocal;
-  	var SerialGammaLocal;
-  	var SerialBetaLocal;
+    
+    var SerialAlphaLocal;
+    var SerialGammaLocal;
+    var SerialBetaLocal;
 
-	var crc;
-	var tdata;
-	var stateM;
-	var nagetiveM;
-	var valueAngle;   
+    var tdata;
+    var nagetiveM;
+    var valueAngle;   
+	  
+    var tsum;
+    var tcount;
+    var tvector;
   		
     let readLoop = () => {
 		
-    crc = 0;
-	stateM = 0;
-      		
     this.device_.transferIn(5, 64).then(result => {
-	
+    if( result.data.byteLength == 4 && result.data.getUint8(0) == 161 )
+    {
+      let view = new Uint8Array(3);
+      view[0] = 56;
+      view[1] = 12;
+      view[2] = 88;
+      return this.device_.transferOut(4, view);
+    }
 
-  	for (var i = 0; i < result.data.byteLength; i++) 
-  	{    
-  	  	tdata = result.data.getUint8(i);
-		console.log(tdata);
-        if(tdata == 168)    //   N ASCII:78
-		{
-		    //console.log("Enter Data Message");
-		    stateM = 1;
-			crc = 168;
-		}
-		else if(stateM == 1)    //   N ASCII:78
-		{
-		    //console.log("Enter N");
-		    stateM = 2;
-			SerialAlphaLocal = tdata;
-			crc = crc + tdata;
-		}
-		else if(stateM == 2)    //   N ASCII:78
-		{
-		    //console.log("Enter Alpha");
-		    stateM = 3;
-            if(tdata >= 100)
-            {
-                SerialAlphaLocal =  SerialAlphaLocal + ((tdata - 100)/100);
-				SerialAlphaLocal =  -SerialAlphaLocal;
-			}
-			else
-			{
-                 SerialAlphaLocal =  SerialAlphaLocal + (tdata/100);
-			}
-			crc = crc + tdata;
-		}
+    if( result.data.byteLength == 34 )
+    {	
+    if( result.data.getUint8(0) == 168 )
+    {
+    	  if(result.data.getUint8(2) >= 100)
+    	  {
+    	  	  SerialAlphaLocal =  result.data.getUint8(1) + ((result.data.getUint8(2) - 100)/100);
+				    SerialAlphaLocal =  -SerialAlphaLocal;
+    	  }
+    	  else
+    	  {
+    	  	  SerialAlphaLocal =  result.data.getUint8(1) + (result.data.getUint8(2)/100);
+    	  }
+    	  
+    	  if(result.data.getUint8(4) >= 100)
+    	  {
+    	  	  SerialBetaLocal =  result.data.getUint8(3) + ((result.data.getUint8(4) - 100)/100);
+				    SerialBetaLocal =  -SerialBetaLocal;
+    	  }
+    	  else
+    	  {
+    	  	  SerialBetaLocal =  result.data.getUint8(3) + (result.data.getUint8(4)/100);
+    	  }
+    	  
+    	  if(result.data.getUint8(6) >= 100)
+    	  {
+    	  	  SerialGammaLocal =  result.data.getUint8(5) + ((result.data.getUint8(6) - 100)/100);
+				    SerialGammaLocal =  -SerialGammaLocal;
+    	  }
+    	  else
+    	  {
+    	  	  SerialGammaLocal =  result.data.getUint8(5) + (result.data.getUint8(6)/100);
+    	  }
+		          	
+		SerialAlpha = SerialAlphaLocal; 
+		SerialBeta  = SerialBetaLocal;  
+		SerialGamma = SerialGammaLocal;  
+	    
+    if(SerialGamma < -180) SerialGamma = -180; if(SerialGamma > 180) SerialGamma = 180;
+    if(SerialBeta < -90) SerialBeta = -90;     if(SerialBeta > 90) SerialBeta = 90;
+    if(SerialAlpha < -180) SerialAlpha = -180; if(SerialAlpha > 0) SerialAlpha = 0;
+    
+    pSerialAlpha++;if(pSerialAlpha<0 || pSerialAlpha>10){pSerialAlpha = 0;}
+    pSerialBeta++; if(pSerialBeta<0  || pSerialBeta>10) {pSerialBeta = 0;}
+    pSerialGamma++;if(pSerialGamma<0 || pSerialGamma>10){pSerialGamma = 0;}
+    
+    if( (SerialAlpha - SerialAlphaLast) < 2 && (SerialAlpha - SerialAlphaLast) > -2) 
+    {
+    	  dateSerialAlpha[pSerialAlpha] =  SerialAlphaLast;
+    }
+    else
+    {
+    	  dateSerialAlpha[pSerialAlpha] =  SerialAlpha;
+    }  
+    
+    if( (SerialBeta - SerialBetaLast) < 5 && (SerialBeta - SerialBetaLast) > -5)
+    {
+    	  dateSerialBeta[pSerialBeta]   =  SerialBetaLast;
+    }
+    else
+    {
+    	  dateSerialBeta[pSerialBeta]   =  SerialBeta;
+    }
 
-		else if(stateM == 3)    //   N ASCII:78
-		{
-		    //console.log("Enter N");
-		    stateM = 4;
-			SerialBetaLocal = tdata;
-			crc = crc + tdata;
-		}
-		else if(stateM == 4)    //   N ASCII:78
-		{
-		    //console.log("Enter Beta");
-		    stateM = 5;
-            if(tdata >= 100)
-            {
-                SerialBetaLocal =  SerialBetaLocal + ((tdata - 100)/100);
-				SerialBetaLocal =  -SerialBetaLocal;
-			}
-			else
-			{
-                 SerialBetaLocal =  SerialBetaLocal + (tdata/100);
-			}
-			crc = crc + tdata;
-		}
+    
+    if( (SerialGamma - SerialGammaLast) < 2 && (SerialGamma - SerialGammaLast) > -2)  
+    {
+    	  dateSerialGamma[pSerialGamma] =  SerialGammaLast;
+    }
+    else
+    {
+    	  dateSerialGamma[pSerialGamma] =  SerialGamma;
+    }
 
-		else if(stateM == 5)    //   N ASCII:78
-		{
-		    //console.log("Enter N");
-		    stateM = 6;
-			SerialGammaLocal = tdata;
-			crc = crc + tdata;
-		}
-		else if(stateM == 6)    //   N ASCII:78
-		{
-		    //console.log("Enter Gamma" + tdata);
-		    stateM = 7;
-            if(tdata >= 100)
-            {
-                SerialGammaLocal =  SerialGammaLocal + ((tdata - 100)/100);
-				SerialGammaLocal =  -SerialGammaLocal;
-			}
-			else
-			{
-                 SerialGammaLocal =  SerialGammaLocal + (tdata/100);
-			}
-			crc = crc + tdata;
-		}
-		else if(stateM == 7)    //   N ASCII:78
-		{
-		    if( (crc%100)== tdata);
-			{
-			    SerialAlpha = SerialAlphaLocal; 
-			    SerialBeta  = SerialBetaLocal;  
-				SerialGamma = SerialGammaLocal;  
-		    }
+    pSerialAlphaMax = 0;
+    pSerialAlphaMin = 0;
+    pSerialBetaMax  = 0;
+    pSerialBetaMin  = 0;
+    pSerialGammaMax = 0;
+    pSerialGammaMin = 0;
+    
+    for(var j=1; j<5; j++)
+    {
+        if(dateSerialAlpha[pSerialAlphaMin] > dateSerialAlpha[j])
+      	{
+      	    pSerialAlphaMin = j;  
+      	}	
+      	if(dateSerialAlpha[pSerialAlphaMax] < dateSerialAlpha[j])
+      	{
+      	    pSerialAlphaMax = j; 
+      	}	
+      	if(dateSerialBeta[pSerialBetaMin] > dateSerialBeta[j])
+      	{
+      	    pSerialBetaMin = j;  
+      	}	
+      	if(dateSerialBeta[pSerialBetaMax] < dateSerialBeta[j])
+      	{
+      	    pSerialBetaMax = j; 
+      	}	
+      	if(dateSerialGamma[pSerialBetaMin] > dateSerialGamma[j])
+      	{
+      	    pSerialGammaMin = j;  
+      	}	
+      	if(dateSerialGamma[pSerialGammaMax] < dateSerialGamma[j])
+      	{
+      	    pSerialGammaMax = j; 
+      	}	
+    }
+    
+    /* Fushion for SerialAlpha */ 
+    tsum = 0;
+    tcount = 0;
+    tvector = 0;
+    for(j=0; j<5; j++)
+    {
+      	 if(j!=pSerialAlphaMin &&  j!=pSerialAlphaMax)
+      	 {
+      	     tvector = tvector + parseInt(dateSerialAlpha[j]); 
+      	     if(dateSerialAlpha[j] < 0)
+      	     {
+      	          tsum = tsum - parseInt(dateSerialAlpha[j]);	
+      	     }
+      	     else
+      	     {	
+      	          tsum = tsum + parseInt(dateSerialAlpha[j]);
+      	     } 
+      	     tcount++; 
+      	  }	
+    }	
+    tsum = tsum / tcount;
+    tsum = tsum.toFixed(2);
+    if(tvector > 0)
+    {
+      	  SerialAlpha = tsum;
+    }
+    else
+    {
+      	  SerialAlpha = -tsum;
+    }
 
-			//var date = new Date();  
-			//console.log(date.getMilliseconds());//??¨¨?¦Ì¡À?¡ã¦Ì?o¨¢??¨ºy
-			//console.log("IMU:" + SerialAlpha + " " + SerialBeta + " " + SerialGamma);
-			stateM = 0;
-		}
-      }	
+    /* Fushion for SerialBeta */  
+    tsum = 0;
+    tcount = 0;
+    tvector = 0;
+    for(j=0; j<5; j++)
+    {
+      	 if(j!=pSerialBetaMin &&  j!=pSerialBetaMax)
+      	 {
+      	     tvector = tvector + parseInt(dateSerialBeta[j]); 
+      	     if(dateSerialBeta[j] < 0)
+      	     {
+      	          tsum = tsum - parseInt(dateSerialBeta[j]);	
+      	     }
+      	     else
+      	     {	
+      	          tsum = tsum + parseInt(dateSerialBeta[j]);
+      	     } 
+      	     tcount++; 
+      	  }	
+    }	
+    tsum = tsum / tcount;
+    tsum = tsum.toFixed(2);
+    if(tvector > 0)
+    {
+      	  SerialBeta = tsum;
+    }
+    else
+    {
+      	  SerialBeta = -tsum;
+    }
+    
+    /* Fushion for SerialGamma */  
+    tsum = 0;
+    tcount = 0;
+    tvector = 0;
+    for(j=0; j<5; j++)
+    {
+      	 if(j!=pSerialGammaMin &&  j!=pSerialGammaMax)
+      	 {
+      	     tvector = tvector + parseInt(dateSerialGamma[j]); 
+      	     if(dateSerialGamma[j] < 0)
+      	     {
+      	          tsum = tsum - parseInt(dateSerialGamma[j]);	
+      	     }
+      	     else
+      	     {	
+      	          tsum = tsum + parseInt(dateSerialGamma[j]);
+      	     } 
+      	     tcount++;
+      	  }
+    }
+    tsum = tsum / tcount;
+    tsum = tsum.toFixed(2);
+    if(tvector > 0)
+    {
+      	  SerialGamma = tsum;
+    }
+    else
+    {
+      	  SerialGamma = -tsum;
+    }
+    
+		SerialAlphaLast = SerialAlpha;
+    SerialBetaLast =  SerialBeta;
+    SerialGammaLast = SerialGamma;
+    //console.log("IMU" + SerialAlpha + " " + SerialBeta + " " + SerialGamma);
+    }
+    }
+   
         this.onReceive(result.data);
         readLoop();
       }, error => {
